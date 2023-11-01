dd780e698f9350e021b23ad846436311e4cd0939
fanticheli <igor.fanticheli@hotmail.com>
2023-11-01 20:37:28 -0300

    Pipeline terraform

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "azurerm_resource_group" "resource" {
 
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   name                = "kubernetes_cluster"
-  location            = azurerm_resource_group.resource_lanchonete.location
-  resource_group_name = azurerm_resource_group.resource_lanchonete.name
+  location            = azurerm_resource_group.resource.location
+  resource_group_name = azurerm_resource_group.resource.name
   dns_prefix          = "lanchonete-creusa"
 
   default_node_pool {
@@ -36,8 +36,8 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
 
 resource "azurerm_container_registry" "container_registry" {
   name                     = "acrlanchonete"
-  resource_group_name      = azurerm_resource_group.resource_lanchonete.name
-  location                 = azurerm_resource_group.resource_lanchonete.location
+  resource_group_name      = azurerm_resource_group.resource.name
+  location                 = azurerm_resource_group.resource.location
   sku                      = "Basic"
   admin_enabled            = false  
 }
