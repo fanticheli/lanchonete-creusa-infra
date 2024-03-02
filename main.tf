027b57fbb9d4ac0b61221fa0061fc8b95bc9d7be
IgorFanticheli <igor.fanticheli@routeasy.com.br>
2024-03-02 17:03:16 -0300

    Teste

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "resource" {
   name     = "resource_lanchonete"
-  location = "eastus2"
+  location = "brazilsouth"
 }
 
 resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
@@ -32,19 +32,4 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
     type = "SystemAssigned"
   }
 
-}
-
-resource "azurerm_container_registry" "container_registry" {
-  name                     = "acrlanchonete"
-  resource_group_name      = azurerm_resource_group.resource.name
-  location                 = azurerm_resource_group.resource.location
-  sku                      = "Premium"
-  admin_enabled            = false  
-}
-
-resource "azurerm_role_assignment" "acr_role" {
-  scope                = azurerm_container_registry.container_registry.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id  
-  skip_service_principal_aad_check = true
 }
